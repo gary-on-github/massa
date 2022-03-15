ff2dd2403cbf173306d6d14ecc9974cec15ddf80
gary-on-github <101473124+gary-on-github@users.noreply.github.com>
2022-03-15 09:38:54 +0000

    fixes

--- massa.sh
+++ massa.sh
@@ -9,11 +9,11 @@ CHOICE=$(
 whiptail --title "Massa Manager" --menu "Make a Choice" 25 78 16 \
 	"1" "Node Installation."   \
 	"2" "Start Client." \
-  "3" "Start Node Service." \
-  "4" "Stop Node Service." \
-  "5" "Create Wallet. - Only run once" \
-  "6" "View wallet." \
-  "7" "Check Journalctl." \
+    "3" "Start Node Service." \
+    "4" "Stop Node Service." \
+    "5" "Create Wallet. - Only run once" \
+    "6" "View wallet." \
+    "7" "Check Journalctl." \
 	"8" "End script"  3>&2 2>&1 1>&3	
 )
 
@@ -59,13 +59,11 @@ sudo ufw allow 31244
 sudo ufw allow 31245 
 sudo ufw allow 22
 sudo ufw --force enable
-;;
 
-ipv6=$(ip -6 addr | grep inet6 | awk -F '[ \t]+|/' '{print $3}' | grep -v ^::1 | grep -v ^fe80)
-sudo tee -a <<EOF >/dev/null "$HOME"/massa/massa-node/config/config.toml
+sudo tee <<EOF >/dev/null "$HOME"/massa/massa-node/config/config.toml
 [network]
 # replace the ip with yours
-routable_ip ="$ipv6"
+routable_ip ="$(hostname -I | cut -d " " -f 2)"
 max_ping = 10000
 # target number of non bootstrap outgoing connections
 target_out_nonbootstrap_connections = 6
@@ -74,20 +72,20 @@ max_in_nonbootstrap_connections = 9
 
 [bootstrap]
 # list of bootstrap (ip, node id)
-bootstrap_list = [["149.202.86.103:31245", "5GcSNukkKePWpNSjx9STyoEZniJAN4U4EUzdsQyqhuP3WYf6nj"],
+bootstrap_list = 
+    [["149.202.86.103:31245", "5GcSNukkKePWpNSjx9STyoEZniJAN4U4EUzdsQyqhuP3WYf6nj"],
     ["149.202.89.125:31245", "5wDwi2GYPniGLzpDfKjXJrmHV3p1rLRmm4bQ9TUWNVkpYmd4Zm"],
     ["158.69.120.215:31245", "5QbsTjSoKzYc8uBbwPCap392CoMQfZ2jviyq492LZPpijctb9c"],
     ["158.69.23.120:31245", "8139kbee951YJdwK99odM7e6V3eW7XShCfX5E2ovG3b9qxqqrq"],
     ["93.29.134.115:31245", "7hx5EnXjTBWUvqDtVuEzyiQQTdNY6zycDi7GVxr7AqZ19Rzg9o"]]
 # refuse consecutive bootstrap attempts from a given IP when the interval between them is lower than per_ip_min_interval milliseconds
 per_ip_min_interval = 3600000
-
 EOF
 echo "Community bootstrap ip addresses added"
-
+;;
 
 2) # 2 - START CLIENT
-cd $HOME/massa/massa-client/
+cd $HOME/massa/massa-client/ || exit
 ./massa-client
 ;;
 
@@ -100,12 +98,12 @@ sudo systemctl stop massa
 ;;
 
 5) # 5 - CREATE WALLET 
-cd $HOME/massa/massa-client/
+cd $HOME/massa/massa-client/ || exit
 ./massa-client --wallet wallet.dat wallet_generate_private_key
 ;;
 
 6) # 6 - VIEW WALLET
-cd $HOME/massa/massa-client/
+cd $HOME/massa/massa-client/ || exit
 ./massa-client -- wallet_info
 ;;
 
